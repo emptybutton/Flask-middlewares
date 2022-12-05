aa5facb474176ab6a33a1c4c5f243544da24208f
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-05 20:33:50 +0700

    ErrorHandler containing other ErrorHandler

--- flask_middlewares/standard/error_handling.py
+++ flask_middlewares/standard/error_handling.py
@@ -1,6 +1,20 @@
 from abc import ABC, abstractmethod
+from typing import Iterable, Self, Callable
 
 
 class IErrorHandler(ABC):
     def __call__(self, error: Exception) -> any:
         pass
+
+
+class ProxyErrorHandler(IErrorHandler):
+    def __init__(self, error_handlers: Iterable[IErrorHandler], *, is_return_delegated: bool = True):
+        self.error_handlers = tuple(error_handlers)
+        self.is_return_delegated = is_return_delegated
+
+    def __call__(self, error: Exception) -> any:
+        for error_handler in self.error_handlers:
+            result = error_handler(error)
+
+            if self.is_return_delegated and result is not None:
+                return result
