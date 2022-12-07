f69c45d7d51778eab34922d9c0d987d5ee55aff2
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-07 19:40:07 +0700

    Documentation for the standard.status_code module

--- flask_middlewares/standard/sql_alchemy.py
+++ flask_middlewares/standard/sql_alchemy.py
@@ -6,11 +6,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 class SQLAlchemyMiddleware(Middleware, ABC):
+    """Middleware class interacting with SQLAlchemy cursor."""
+
     def __init__(self, database: SQLAlchemy):
         self.database = database
 
 
 class SQLAlchemySessionFinisherMiddleware(SQLAlchemyMiddleware):
+    """
+    SQLAlchemyMiddleware class that implements the final commit to end the
+    router session, or roll it back.
+    """
+    
     def call_route(self, route: Callable, *args, **kwargs) -> any:
         try:
             result = route(*args, **kwargs)
