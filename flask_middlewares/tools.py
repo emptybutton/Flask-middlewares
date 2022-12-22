088e15ce4cc275a4c4521e9aa588a570421c58a2
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-22 22:23:06 +0700

    Class for checking whether a data type matches some other type

--- flask_middlewares/tools.py
+++ flask_middlewares/tools.py
@@ -101,6 +101,39 @@ class BinarySet(StylizedMixin):
             included if self.included is not None or other.included is not None else None,
             non_included if self.non_included is not None or other.non_included is not None else None
         )
+
+
+class TypeDeterminant:
+    """
+    Class that implements checking whether an object conforms to certain types
+
+    Has the is_correctness_under_supertype flag attribute that specifies whether
+    the object type should match the all support types.
+    """
+
+    def __init__(
+        self,
+        correct_type_resource: Iterable[Exception] | Exception,
+        *,
+        is_correctness_under_supertype: bool = False
+    ):
+        self.is_correctness_under_supertype = is_correctness_under_supertype
+        self.correct_types = (
+            correct_type_resource
+            if isinstance(correct_type_resource, Iterable)
+            else (correct_type_resource, )
+        )
+
+    def __call__(self, object_: any) -> bool:
+        return (
+            len(self.correct_types) == 0
+            and (all if self.is_correctness_under_supertype else any)(
+                isinstance(object_, correct_type)
+                for correct_type in self.correct_types
+            )
+        )
+
+
 def parse_config_from(
     file_name: str,
     config_parse_method: Callable[[Config, str], None]=Config.from_object
